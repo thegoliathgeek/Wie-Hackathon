84bff258314030a05c74d98697f74356c00c29a0
imdhanush <dhanushdazz@gmail.com>
2019-10-12 11:52:28 +0530

    database update

--- python-firebase/app.py
+++ python-firebase/app.py
@@ -1,8 +1,12 @@
 import pyrebase
 from credentials import firebaseConfig
+import os
 
+path = r'/home/joker/Programs/wie-hackathon/python-firebase/'
 firebase = pyrebase.initialize_app(firebaseConfig)
 
+database = firebase.database()
 storage = firebase.storage()
 
-storage.child('some.jpeg').put('jok.jpeg')
+# storage.child('Images/jok.jpeg').put(path+r'Images/jok22.jpeg')
+database.child('imName').set('Dazz')
