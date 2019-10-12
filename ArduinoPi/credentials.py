96136afc1e891c5efb037e7245f91096fa6e1592
rishithSadashiv <rishith.1si16ec078@gmail.com>
2019-10-12 05:57:26 -0900

    Pi Update

---
+++ ArduinoPi/credentials.py
@@ -0,0 +1,9 @@
+firebaseConfig = {
+    'apiKey': "AIzaSyB1WMISjeCDHWYijpJ1sRyMJvhqHxfRiPc",
+    'authDomain': "badhackathon.firebaseapp.com",
+    'databaseURL': "https://badhackathon.firebaseio.com",
+    'projectId': "badhackathon",
+    'storageBucket': "badhackathon.appspot.com",
+    'messagingSenderId': "922261326043",
+    'appId': "1:922261326043:web:5c81ad71e3be7b6eb424fe"
+  }
